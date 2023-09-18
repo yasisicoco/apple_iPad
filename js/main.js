e263ccf64d15940abdae7dec9536debea44005ce
jinwoo <yasisicoco@gmail.com>
2023-09-18 22:35:41 +0900

    '16분40초'

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = basketStarterEl.querySelector('.basket')
+
+basketStarterEl.addEventListener('click', function () {
+  if (basketEl.classList.contains('show')) {
+    // hide
+  } else {
+    // show
+  }
+})
